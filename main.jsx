ebfee5acd85545f9b3ce81e5497823c12b439f69
titigamer1468-design <titigamer1468@gmail.com>
2026-07-08 16:34:36 -0400

    Renombro AppUI a .js para resolución final

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppUI from "./AppUI.js"; // Asegúrate de que termine en .js
+import AppUI from "./AppUI.jsx"; // Cambiamos './src/AppUI.jsx' por './AppUI.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppUI />
