ab04b7997bd06cc2eb86c5bec4f0722b5166727c
titigamer1468-design <titigamer1468@gmail.com>
2026-07-08 16:23:51 -0400

    Estructura plana total: archivos fuera de src

--- main.jsx
+++ main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppUI from './AppUI'; // Asegúrate de que el archivo se llame AppUI.jsx o AppUI.js
-
+import AppUI from "./AppUI.jsx"; // Cambiamos './src/AppUI.jsx' por './AppUI.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppUI />
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
     rollupOptions: {
-      input: {
-        main: './index.html'
-      }
+      input: './index.html'
     }
   }
 })
