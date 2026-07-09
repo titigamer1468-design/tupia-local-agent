e149770061d1732c88ebd6c16e3528b331276029
titigamer1468-design <titigamer1468@gmail.com>
2026-07-09 10:15:54 -0400

    Regresando seguridad a require-corp para activar motor FFmpeg

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   ],
   server: {
     headers: {
-      "Cross-Origin-Embedder-Policy": "credentialless",
+      "Cross-Origin-Embedder-Policy": "require-corp",
       "Cross-Origin-Opener-Policy": "same-origin",
     },
   },
